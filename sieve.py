92a914cb65279a2733ed61cb4e25d81654f09bf5
satorhho <lbriones@gbox.adnu.edu.ph>
2020-06-01 23:44:41 +0800

    Python implementation

---
+++ sieve.py
@@ -0,0 +1,17 @@
+upperbound = int(input("Ender upper bound:  "))
+A = [i for i in range(2, upperbound + 1)]
+
+size = upperbound - 1
+i = 0
+while i < size:
+    denominator = A[i]
+    j = i +1
+    while (j < size):
+        if not (A[j] % denominator):
+            A.pop(j)
+            size -= 1
+        else:
+            j += 1
+    i += 1
+
+print(A)
